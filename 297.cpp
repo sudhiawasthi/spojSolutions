cd19f5ab08c8a08a1c2658a73df3c1fa2ad997df
Sudhi Awasthi <35043619+sudhiawasthi@users.noreply.github.com>
2019-10-22 07:48:01 +0530

    Update 297.cpp

--- 297.cpp
+++ 297.cpp
@@ -1,16 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main()
-{  
-int t;
-cin>>t;
-init();
-while(t--)
+bool F(ll x ,ll arr[],int c,int n)
 {
-int n,c;
+   
+//We will start placing cows from the first stall and check if the next stall can be
+//used on not
+ 
+int last=arr[0],cows=1;
+for(int i=1;i<n;i++)
+{
+if(arr[i]-last>=x)
+{
+    last=arr[i];
+    cows++;
+    if(cows==c)
+    return 1;
+}
+}
+return 0;
+}
+ 
+int main()
+{ int t;
+cin>>t;
+while(t--)
+{  int n,c;
 cin>>n>>c;
-int loc[n];
+ll arr[n];
 for(int i=0;i<n;i++)
-cin>>loc[i];
-
+cin>>arr[i];
+sort(arr,arr+n);
+ll mingap = 0;
+ll maxgap= arr[n-1]-arr[0]+1;
+ 
+while(mingap<maxgap)
+{
+    ll mid = (mingap+maxgap+1)/2;
+    if(F(mid,arr,c,n))
+    mingap=mid;
+    else 
+    maxgap=mid-1;
+}
+cout<<mingap<<endl;
+ 
+}
 } 
